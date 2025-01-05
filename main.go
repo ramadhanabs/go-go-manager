44392f7168e978a0602c7f6dab9362dfaedbe77c
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2025-01-05 14:24:06 +0700

    chore: initial commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "success",
+		})
+	})
+
+	r.Run(":8888")
+}
